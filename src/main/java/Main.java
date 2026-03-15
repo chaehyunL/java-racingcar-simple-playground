b1a681f3f9960fa6965e0c84195be2e5eb5debc6
chaehyunL <chaehyun1012@gmail.com>
2026-03-15 20:03:49 +0900

    Car 함수 생성 Get Winners 생성

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        Car A = new Car("A");
-        System.out.println("자동차 이름:"+A.name);
-        System.out.println("위치:"+A.position);
-        A.move();
-        System.out.println("위치"+A.position);
-    }
 
+    }
 }
