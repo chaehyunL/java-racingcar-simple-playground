835c3de31f990aa03042a596ab92987c4d69c47f
chaehyunL <chaehyun1012@gmail.com>
2026-03-22 09:50:09 +0900

    style: 자바 가이드 스타일로 변경

--- src/main/java/RaceResult.java
+++ src/main/java/RaceResult.java
@@ -7,12 +7,13 @@ public class RaceResult {
     public RaceResult(List<Car> newCars) {
         this.cars = new ArrayList<>(newCars);
     }
+
     public List<Car> getResult() {
         List<Car> resultCars = new ArrayList<>();
-        int maxPosition=cars.stream()
+        int maxPosition = cars.stream()
                 .mapToInt(Car::getPosition).max()
                 .orElse(0);
-        resultCars=cars.stream().filter(car->car.getPosition()==maxPosition)
+        resultCars = cars.stream().filter(car -> car.getPosition() == maxPosition)
                 .toList();
         return resultCars;
     }
