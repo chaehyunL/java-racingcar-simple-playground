42cc98b2edf79e4aa30cdd4976ad791af78c850a
chaehyunL <chaehyun1012@gmail.com>
2026-03-26 10:02:56 +0900

    test:display 이름 변경.

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 
 public class CarTest {
     @Test
-    @DisplayName("Car 객체, 이름 반환 test")
+    @DisplayName("Car이름을 A로 설정하면_이름이 A로 호출된다")
     public void Test_getName() {
         Car car = new Car("A");
         Assertions.assertEquals("A", car.getName());
     }
 
     @Test
-    @DisplayName("정지 test[0-3]")
+    @DisplayName("자동차가 0-3 사이 값이 나오면 정지함.")
     public void Test_underForwardThreshold() {
         Car car = new Car("A");
         car.move(3);
@@ -22,7 +22,7 @@ public class CarTest {
     }
 
     @Test
-    @DisplayName("전진 test[4-9]")
+    @DisplayName("자동차가 4-9사이의 값이 나오면 전진함.")
     public void Test_overForwardThreshold() {
         Car car = new Car("A");
         car.move(4);
