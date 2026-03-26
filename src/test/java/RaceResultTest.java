fd56e513dc65079b007a7245957b5d8aa2caf994
chaehyunL <chaehyun1012@gmail.com>
2026-03-26 10:07:37 +0900

    test:given-when-then으로 변경

--- src/test/java/RaceResultTest.java
+++ src/test/java/RaceResultTest.java
@@ -12,11 +12,13 @@ public class RaceResultTest {
     @DisplayName("우승자 1명인 경우 올바르게 추출")
     public void Test_singlewinner() {
         Car car1 = new Car("A");
-        car1.move(4);
         Car car2 = new Car("B");
+
+        car1.move(4);
         car2.move(0);
         RaceResult raceResult = new RaceResult(List.of(car1, car2));
         List<Car> winners = raceResult.getResult();
+
         assertThat(winners).hasSize(1);
         assertThat(winners.get(0).getName()).isEqualTo("A");
     }
@@ -25,10 +27,10 @@ public class RaceResultTest {
     @DisplayName("공동우승자 추출")
     public void Test_multiwinners() {
         Car car1 = new Car("A");
-        car1.move(4);
         Car car2 = new Car("B");
-        car2.move(4);
 
+        car1.move(4);
+        car2.move(4);
         RaceResult raceResult = new RaceResult(List.of(car1, car2));
         List<Car> winners = raceResult.getResult();
 
