ff445232733662d7893cd7d6e68cfa20f48260cf
chaehyunL <chaehyun1012@gmail.com>
2026-03-26 23:44:03 +0900

    test:given-when-then 형식으로 변경

--- src/test/java/RaceResultTest.java
+++ src/test/java/RaceResultTest.java
@@ -11,14 +11,15 @@ public class RaceResultTest {
     @Test
     @DisplayName("우승자 1명인 경우 올바르게 추출")
     public void Test_singlewinner() {
+        //given:'A'와 'B'라는 자동차가 주어질때
         Car car1 = new Car("A");
         Car car2 = new Car("B");
-
+        //when:'A'자동차는 4를 부여받고, 'B'자동차는 0을 부여받을때
         car1.move(4);
         car2.move(0);
         RaceResult raceResult = new RaceResult(List.of(car1, car2));
         List<Car> winners = raceResult.getResult();
-
+        //Then:우승자는 A로 결정된다.
         assertThat(winners).hasSize(1);
         assertThat(winners.get(0).getName()).isEqualTo("A");
     }
@@ -26,14 +27,15 @@ public class RaceResultTest {
     @Test
     @DisplayName("공동우승자 추출")
     public void Test_multiwinners() {
+        //given:'A'와 'B'라는 차가 주어질때,
         Car car1 = new Car("A");
         Car car2 = new Car("B");
-
+        //when:'A'와 'B'모두 4숫자를 부여받을때
         car1.move(4);
         car2.move(4);
         RaceResult raceResult = new RaceResult(List.of(car1, car2));
         List<Car> winners = raceResult.getResult();
-
+        //then:우승자는 'A','B' 모두로 결정된다
         assertThat(winners).hasSize(2);
         assertThat(winners).extracting(Car::getName).containsExactlyInAnyOrder("A", "B");
     }
