8ad509c65e431df3a18a6c26422962020e0ecce5
chaehyunL <chaehyun1012@gmail.com>
2026-03-26 10:10:13 +0900

    refector: number 변수 위치 변경

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -10,12 +10,12 @@ public class Controller {
     public void run() {
         String input = InputView.getCarNames();
         String[] carNames = input.split(",");
-        int number = InputView.getTrynumber();
 
         List<Car> cars = new ArrayList<>();
         for (String name : carNames) {
             cars.add(new Car(name.trim()));
         }
+        int number = InputView.getTrynumber();
         RacingGame racingGame= new RacingGame(cars);
 
         OutputView.getOutput();
